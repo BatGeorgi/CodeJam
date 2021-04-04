5c5a9ced3f385d6f7fb6d78567c0cc18c7407604
BatGeorgi <7sparkle77@gmail.com>
2021-04-04 14:18:46 +0300

    imp

--- src/round1C_2019_B/Solution.java
+++ src/round1C_2019_B/Solution.java
@@ -1,4 +1,4 @@
-package round2_2020;
+package round1C_2019_B;
 
 import java.util.Scanner;
 
@@ -18,86 +18,26 @@ public class Solution {
 		if (multiCase) {
 			int T = sc.nextInt();
 			for (int tc = 1; tc <= T; ++tc) {
-				String pref = "Case #" + tc + ": ";
+				out("Case #" + tc + ": ");
 				Solution s = new Solution(sc);
 				try {
-					Object result = s.solve();
-					out(pref + result);
+					s.solve();
 				} catch (Throwable t) {
-					out(pref + t.getMessage());
 					t.printStackTrace();
 				}
 			}
 		} else {
 			Solution s = new Solution(sc);
 			try {
-				Object result = s.solve();
-				out(result);
+				s.solve();
 			} catch (Throwable t) {
 				t.printStackTrace();
 			}
 		}
 	}
 
-	private Object solve() {
-		long L = sc.nextLong(), R = sc.nextLong();
-		long D = Math.abs(L - R);
-		double rt = Math.sqrt(2 * D);
-		long f = (long) rt;
-		if (f * (f + 1) > 2 * D) {
-			--f;
-		}
-		long n = f;
-		if (L < R) {
-			R -= (f * (f + 1)) / 2;
-		} else {
-			L -= (f * (f + 1)) / 2;
-		}
-		if (L < R) {
-			long k = findK(R, f);
-			if (k > 0) {
-				n = f + k * 2 - 1;
-				long remR = k * k + f * k;
-				R -= remR;
-				long remL = remR - k - f;
-				L -= remL;
-			}
-		} else {
-			long k = findK(L, f);
-			if (k > 0) {
-				n = f + k * 2 - 1;
-				long remL = k * k + f * k;
-				L -= remL;
-				long remR = remL - k - f;
-				R -= remR;
-			}
-		}
-		if (L >= n + 1 || R >= n + 1) {
-			++n;
-			if (L >= R) {
-				L -=n;
-			} else {
-				R -= n;
-			}
-		}
-		return (n + " " + L + " " + R);
-	}
-
-	long findK(long R, long f) {
-		long lb = 0, rb = 1000000000L + 1;
-		for (int i = 0; i < 34; ++i) {
-			long k = (lb + rb) / 2;
-			long res = k * k + f * k;
-			if (res == R) {
-				return k;
-			}
-			if (res > R) {
-				rb = k;
-			} else {
-				lb = k;
-			}
-		}
-		return lb;
+	private void solve() {
+		// TODO solve case here
 	}
 
 	/* UTIL classes and methods */
